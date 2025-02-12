4f159dea59031ca9f73aaccae102ade2c890c49e
chauhan-09 <nsc7574@gmail.com>
2025-02-12 19:01:53 +0530

    made changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5500;
 
 app.get('/', (req,res) => {
     res.send('Home Page!');
@@ -9,4 +11,28 @@ app.get('/', (req,res) => {
 app.listen(PORT, ()=> {
     console.log('Server is running');
 })
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    secure:true,
+    port:465,
+    auth: {
+        user:process.env.sender_email,
+        pass:process.env.pass,
+    },
+});
+
+function sendmail(to,sub,msg){
+    transporter.sendMail({
+        to:to,
+        subject:sub,
+        html:msg
+    });
+
+    console.log('email sent');
+}
+
+sendmail("nsc7574@gmail.com","Congratulations","You have Reached 2000 elo in chess");
+
+
 
